717b26ef1674d67f927dca7e068275f2a54525ed
Smriti Singh <thesmritisingh11@gmail.com>
2026-07-16 21:40:37 +0530

    Add program to check if a number is positive or negative

---
+++ q5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{int n;
+cout<<"enter number"<<endl;
+cin>>n;
+if(n>=0){
+    cout<<"positive";
+    
+}
+else{
+    cout<<"negative";
+}
+
+    return 0;
+}
