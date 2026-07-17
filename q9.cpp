babbe2e18462c996ad664772d6c65b8974e53b0e
Smriti Singh <thesmritisingh11@gmail.com>
2026-07-18 00:59:33 +0530

    Add C++ program to print alphabet pattern

---
+++ q9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{char ch='A';
+int i,j;
+int n;
+cout<<"enter no ";
+cin>>n;
+for(i=0;i<n;i++){
+    for(j=0;j<i+1;j++){
+        cout<<ch;
+       
+    }ch++;
+    cout<<endl;
+}
+    return 0;
+}
